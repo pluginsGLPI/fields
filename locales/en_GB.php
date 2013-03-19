20cee3532a1cd9cef36dd0a3b906ccc86c089b0d
Alexandre Delaunay <adelaunay@teclib.com>
2013-03-19 15:45:16 +0100

    update locales

--- locales/en_GB.php
+++ locales/en_GB.php
@@ -3,4 +3,30 @@
 $LANG['fields']['title'][1]="Additionnal fields";
 
 $LANG['fields']['install'][0] = "MySQL tables installation";
+$LANG['fields']['install'][1]   = "Les valeurs du plugin Customfields ont été importé dans un dumps, rendez vous dans le menu Administration -> maintenance pour l'importer";
 $LANG['fields']['uninstall'][0] = "MySQL tables uninstallation";
+
+$LANG['fields']['error']['dir_write'] = "Ce plugin nécéssite les droits d'écritures sur ses propres fichiers, veuillez corriger.";
+
+$LANG['fields']['config']['fields']     = "Configurate the fields";
+$LANG['fields']['config']['containers'] = "Configurate the blocs";
+
+$LANG['fields']['type'][0] = "Field";
+$LANG['fields']['type'][1] = "Bloc";
+
+$LANG['fields']['types'][0] = "Fields";
+
+$LANG['fields']['container']['type']['tab'] = "Add tab";
+$LANG['fields']['container']['type']['dom'] = "Insertion in the form (before save button)";
+
+$LANG['fields']['field']['type']['header']   = "Header";
+$LANG['fields']['field']['type']['text']     = "Text (single line)";
+$LANG['fields']['field']['type']['textarea'] = "Text (multiples lines)";
+$LANG['fields']['field']['type']['number']   = "Number";
+$LANG['fields']['field']['type']['dropdown'] = "Dropdown";
+$LANG['fields']['field']['type']['yesno']    = "Yes/No";
+$LANG['fields']['field']['type']['date']     = "Date";
+$LANG['fields']['field']['type']['datetime'] = "Date & time";
+
+$LANG['fields']['field']['label']['add']       = "Add a new field";
+$LANG['fields']['field']['label']['no_fields'] = "No field for this bloc";
