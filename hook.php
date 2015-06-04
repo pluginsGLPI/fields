f11c8a8c9c4281534a8712b6ef53115ab73501ab
Alexandre Delaunay <adelaunay@teclib.com>
2015-06-04 09:43:52 +0200

    more corrections for #59, empty dates

--- hook.php
+++ hook.php
@@ -212,3 +212,19 @@ function plugin_fields_rule_matched($params) {
       break;
    }
 }
+
+function plugin_fields_giveItem($itemtype,$ID,$data,$num) {
+   $searchopt = &Search::getOptions($itemtype);
+   $table = $searchopt[$ID]["table"];
+   $field = $searchopt[$ID]["field"];
+   $datatype = $searchopt[$ID]["datatype"];
+   
+   //fix glpi default Search::giveItem who for empty date display "--"
+   if (strpos($table, "glpi_plugin_fields") !== false 
+       && strpos($datatype, "date") !== false
+       && empty($data['raw']["ITEM_$num"])) {
+      return " ";
+   }
+
+   return false;
+}
