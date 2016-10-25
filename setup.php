809b0555f54659fad27a6ed1bdc750c3985ffa51
Johan Cwiklinski <jcwiklinski@teclib.com>
2016-10-25 08:37:39 +0200

    Add missing licence header

--- setup.php
+++ setup.php
@@ -1,4 +1,32 @@
 <?php
+/*
+ * @version $Id: HEADER 15930 2011-10-30 15:47:55Z tsmr $
+ -------------------------------------------------------------------------
+ Fields plugin for GLPI
+ Copyright (C) 2016 by the fields Development Team.
+
+ https://forge.indepnet.net/projects/mreporting
+ -------------------------------------------------------------------------
+
+ LICENSE
+
+ This file is part of fields.
+
+ fields is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ fields is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with fields. If not, see <http://www.gnu.org/licenses/>.
+ --------------------------------------------------------------------------
+ */
+
 
 if (!defined("PLUGINFIELDS_DIR")) {
    define("PLUGINFIELDS_DIR", GLPI_ROOT . "/plugins/fields");
