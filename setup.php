d1674c99e36feab39d1e75090863127f6d63aa48
Johan Cwiklinski <jcwiklinski@teclib.com>
2016-12-30 08:04:52 +0100

    Bump version, update changelog

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
 
-define ('PLUGIN_FIELDS_VERSION', '1.4.2');
+define ('PLUGIN_FIELDS_VERSION', '1.4.3');
 
 if (!defined("PLUGINFIELDS_DIR")) {
    define("PLUGINFIELDS_DIR", GLPI_ROOT . "/plugins/fields");
