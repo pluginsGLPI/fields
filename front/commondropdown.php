c32575722de354c83b91b205598225bf382366e0
tomolimo <olivier.moron@araymond.com>
2017-02-27 15:51:58 +0100

    Added normalization of backslashes
    To be sure that backslashes and slashes are in the same direction in realpath and frontpath.
    Otherwise the test fails each time.

--- front/commondropdown.php
+++ front/commondropdown.php
@@ -2,7 +2,9 @@
 
 include "../../../inc/includes.php";
 $path = PLUGINFIELDS_FRONT_PATH . '/' . $_REQUEST['ddtype'] . '.php';
-if (strpos(realpath($path), realpath(PLUGINFIELDS_FRONT_PATH)) === 0) {
+$realpath = str_replace( "\\", "/", realpath($path));
+$frontpath = str_replace( "\\", "/", PLUGINFIELDS_FRONT_PATH );
+if (strpos($realpath, $frontpath) === 0) {
     include_once $path;
 } else {
     throw new \RuntimeException('Attempt to load unsecure or missing ' . $path .'!');
