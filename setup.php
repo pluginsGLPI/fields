3071f78803a6f39b5c151b846b62c54630982275
Alexandre Delaunay <delaunay.alexandre@gmail.com>
2017-03-03 15:06:01 +0100

    bump version
    fix xml

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
 
-define ('PLUGIN_FIELDS_VERSION', '1.5.0');
+define ('PLUGIN_FIELDS_VERSION', '1.6.0');
 
 if (!defined("PLUGINFIELDS_DIR")) {
    define("PLUGINFIELDS_DIR", GLPI_ROOT . "/plugins/fields");
