c81ef01e72aa05581c14e6b4bd3a510a29a484b6
Cédric Anne <cedric.anne@gmail.com>
2018-05-25 10:33:47 +0200

    Add 1.7.2 version definition

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
 
-define ('PLUGIN_FIELDS_VERSION', '1.7.1');
+define ('PLUGIN_FIELDS_VERSION', '1.7.2');
 
 if (!defined("PLUGINFIELDS_DIR")) {
    define("PLUGINFIELDS_DIR", GLPI_ROOT . "/plugins/fields");
