7b416b9cc875b6ace40069c71f191d281d4fbd0f
Stanislas <skita@teclib.com>
2020-10-15 10:13:58 +0200

    refactor code

--- inc/inventory.class.php
+++ inc/inventory.class.php
@@ -35,12 +35,7 @@ class PluginFieldsInventory extends CommonDBTM {
 
                      $arrayinventory = unserialize(gzuncompress($db_info->fields['serialized_inventory']));
                      if (isset($arrayinventory['custom'])) {
-                        if (isset($arrayinventory['custom']['container']['ID'])) {
-                           $customData['container'][0] = $arrayinventory['custom']['container'];
-                        } else {
-                           $customData = $arrayinventory['custom'];
-                        }
-                        self::updateFields($customData, $itemtype, $items_id);
+                        self::updateFields($arrayinventory['custom']['container'], $itemtype, $items_id);
                      }
                   }
                   //Load XML file because FI always update XML file and don't store inventory into DB
@@ -49,12 +44,7 @@ class PluginFieldsInventory extends CommonDBTM {
                   if ($file !== false) {
                      $arrayinventory = PluginFusioninventoryFormatconvert::XMLtoArray($file);
                      if (isset($arrayinventory['CUSTOM'])) {
-                        if (isset($arrayinventory['CUSTOM']['CONTAINER']['ID'])) {
-                           $customData['container'][0] = $arrayinventory['CUSTOM']['CONTAINER'];
-                        } else {
-                           $customData['container'] = $arrayinventory['CUSTOM']['CONTAINER'];
-                        }
-                        self::updateFields($customData, $itemtype, $items_id);
+                        self::updateFields($arrayinventory['CUSTOM']['CONTAINER'], $itemtype, $items_id);
                      }
                   }
                }
@@ -63,16 +53,19 @@ class PluginFieldsInventory extends CommonDBTM {
       }
    }
 
-   static function updateFields($customData, $itemtype, $items_id) {
-      foreach ($customData['container'] as $key => $dataContainers) {
+   static function updateFields($containersData, $itemtype, $items_id) {
+      if (isset($containersData['ID'])) {
+         // $containersData contains only one element, encapsulate it into an array
+         $containersData = [$containersData];
+      }
+      foreach ($containersData as $key => $containerData) {
          $container = new PluginFieldsContainer();
-         $container->getFromDB($dataContainers['ID']);
-
+         $container->getFromDB($containerData['ID']);
          $data = [];
          $data["items_id"] = $items_id;
          $data["itemtype"] = $itemtype;
-         $data["plugin_fields_containers_id"] = $dataContainers['ID'];
-         foreach ($dataContainers['FIELDS'] as $key => $value) {
+         $data["plugin_fields_containers_id"] = $containerData['ID'];
+         foreach ($containerData['FIELDS'] as $key => $value) {
             $data[strtolower($key)] = $value;
          }
          $container->updateFieldsValues($data, $itemtype, false);
@@ -88,22 +81,13 @@ class PluginFieldsInventory extends CommonDBTM {
       }
 
       //Check if the file exists with the .xml extension (new format)
-      $file           = PLUGIN_FUSIONINVENTORY_XML_DIR;
-      $filename       = $items_id.'.xml';
-      $file_shortname = strtolower($itemtype)."/".$folder."/".$filename;
-      $file          .= $file_shortname;
-      if (!file_exists($file)) {
-         //The file doesn't exists, check without the extension (old format)
-         $file           = PLUGIN_FUSIONINVENTORY_XML_DIR;
-         $filename       = $items_id;
-         $file_shortname = strtolower($itemtype)."/".$folder."/".$filename;
-         $file          .= $file_shortname;
-         if (file_exists($file)) {
-            $pxml = @simplexml_load_file($file, 'SimpleXMLElement', LIBXML_NOCDATA);
-         }
-      } else {
-         $pxml = @simplexml_load_file($file, 'SimpleXMLElement', LIBXML_NOCDATA);
+      $file = PLUGIN_FUSIONINVENTORY_XML_DIR . strtolower($itemtype) . "/" . $folder . "/" . $items_id;
+      if (file_exists($file . '.xml')) {
+         $file .= '.xml';
+      } else if (!file_exists($file)) {
+         return false;
       }
+      $pxml = simplexml_load_file($file, 'SimpleXMLElement', LIBXML_NOCDATA);
       return $pxml;
    }
 
