51a8e8fc3df268d2465d00077be986c453c76e92
tomolimo <olivier.moron@araymond.com>
2021-07-12 11:04:34 +0200

    Fix default value in massive actions

--- inc/field.class.php
+++ inc/field.class.php
@@ -875,7 +875,7 @@ class PluginFieldsField extends CommonDBTM {
                                         $searchOption['linkfield']);
 
       //find field
-      $query = "SELECT fields.plugin_fields_containers_id, fields.is_readonly
+      $query = "SELECT fields.plugin_fields_containers_id, fields.is_readonly, fields.default_value
                 FROM glpi_plugin_fields_fields fields
                 LEFT JOIN glpi_plugin_fields_containers containers
                   ON containers.id = fields.plugin_fields_containers_id
@@ -897,7 +897,8 @@ class PluginFieldsField extends CommonDBTM {
          'type'                        => $searchOption['pfields_type'],
          'plugin_fields_containers_id' => $data['plugin_fields_containers_id'],
          'name'                        => $cleaned_linkfield,
-         'is_readonly'                 => $data['is_readonly']
+         'is_readonly'                 => $data['is_readonly'],
+         'default_value'               => $data['default_value']
       ]];
 
       //show field
