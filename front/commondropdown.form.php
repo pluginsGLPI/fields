0ddca148c434957c3c51fb3e24ba7cd72803f2d3
AdrienClairembault <aclairembault@teclib.com>
2021-11-08 09:47:09 +0100

    Fix path check2

--- front/commondropdown.form.php
+++ front/commondropdown.form.php
@@ -3,7 +3,7 @@
 include "../../../inc/includes.php";
 $path = PLUGINFIELDS_FRONT_PATH . '/' . $_REQUEST['ddtype'] . '.form.php';
 $realpath = str_replace( "\\", "/", realpath($path));
-$frontpath = str_replace( "\\", "/", PLUGINFIELDS_FRONT_PATH );
+$frontpath = str_replace("\\", "/", realpath(PLUGINFIELDS_FRONT_PATH));
 if (strpos($realpath, $frontpath) === 0) {
     include_once $path;
 } else {
