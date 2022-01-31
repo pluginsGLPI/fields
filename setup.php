1f41dda52dbae217f6971544734e0e4a0962e0d3
Cédric Anne <cedric.anne@gmail.com>
2022-01-31 13:50:48 +0100

    Add 1.12.9 version definition

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define ('PLUGIN_FIELDS_VERSION', '1.12.8');
+define ('PLUGIN_FIELDS_VERSION', '1.12.9');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "9.5");
