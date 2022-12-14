b18405ac3af4b2c820d9cb6063d2ca04f1f30999
tomolimo <olivier.moron@araymond.com>
2022-12-14 10:32:37 +0100

    To be able to add custom fields to KnowbaseItem class
    To be able to add custom fields to KnowbaseItem class

--- inc/toolbox.class.php
+++ inc/toolbox.class.php
@@ -259,6 +259,7 @@ class PluginFieldsToolbox
             ProjectTask::class,
             Reminder::class,
             RSSFeed::class,
+            KnowbaseItem::class,
         ];
 
         $administration_itemtypes = [
