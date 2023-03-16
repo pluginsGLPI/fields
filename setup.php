94772f4d064390f9d004a170c3e3247859a384b5
Cédric Anne <cedric.anne@gmail.com>
2023-03-16 10:43:09 +0100

    1.20.2

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_FIELDS_VERSION', '1.20.1');
+define('PLUGIN_FIELDS_VERSION', '1.20.2');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "10.0.0");
