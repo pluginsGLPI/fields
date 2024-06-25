532096a2b88cb21bac0c54435bb6d43e31cf6577
Rom1-B <rbiot@teclib.com>
2024-06-25 11:07:08 +0200

    1.21.10

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_FIELDS_VERSION', '1.21.9');
+define('PLUGIN_FIELDS_VERSION', '1.21.10');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "10.0.0");
