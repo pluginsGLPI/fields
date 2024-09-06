b01f7814b390d8b028d4c266dc7c93905216fd9c
Stanislas <skita@teclib.com>
2024-09-06 14:58:39 +0200

    1.21.12

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_FIELDS_VERSION', '1.21.11');
+define('PLUGIN_FIELDS_VERSION', '1.21.12');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_FIELDS_MIN_GLPI", "10.0.0");
