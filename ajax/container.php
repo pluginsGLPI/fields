34e52bc0ec1ce00ca8011bf4b5225f858f366362
akstis-typer <nikola22sat@mail.ru>
2025-05-06 09:39:41 +0300

    Revert "Revert changes"
    This reverts commit 1fad33246f6420de2bb19f201843c80cef21dfba.

--- ajax/container.php
+++ ajax/container.php
@@ -66,7 +66,7 @@ if (isset($_GET['action']) && $_GET['action'] === 'get_fields_html') {
             $item,
             $type,
             $subtype,
-            //$field_options
+            $field_options
         );
         //echo "</div>";
     } else {
