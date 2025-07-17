255eebaf2c8c47ccb4ba64798539bd3fd6a68f7f
Rom1-B <rom1.biot@gmail.com>
2025-07-17 10:11:00 +0200

    beta2

--- setup.php
+++ setup.php
@@ -31,7 +31,7 @@
 /** @var array $CFG_GLPI */
 global $CFG_GLPI;
 
-define('PLUGIN_FIELDS_VERSION', '1.22.0-beta1');
+define('PLUGIN_FIELDS_VERSION', '1.22.0-beta2');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_FIELDS_MIN_GLPI', '11.0.0');
