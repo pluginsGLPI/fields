9fc1624c5056264ed8fb63528504bd39e88b88d1
Langlois Gaëtan <64356364+MyvTsv@users.noreply.github.com>
2025-07-23 14:08:02 +0200

    Update inc/abstractcontainerinstance.class.php
    Co-authored-by: Romain B. <8530352+Rom1-B@users.noreply.github.com>

--- inc/abstractcontainerinstance.class.php
+++ inc/abstractcontainerinstance.class.php
@@ -158,10 +158,13 @@ abstract class PluginFieldsAbstractContainerInstance extends CommonDBChild
                 && $field_specs->fields['multiple']
             ) {
                 $itemtype = PluginFieldsDropdown::getClassname($field_specs->fields['name']);
-                if (empty($values[$field]) && empty($field_specs->fields["default_value"])) {
-                    return ''; // Value not defined
-                } elseif (empty($values[$field]) && !empty($field_specs->fields["default_value"])) {
-                    $values[$field] = $field_specs->fields['default_value'];
+                if (empty($values[$field]) {
+                    if (!empty($field_specs->fields["default_value"])) {
+                        $values[$field] = $field_specs->fields['default_value'];
+                    } else {
+                        return ''; // Value not defined
+                    }
+                }
                 }
                 $values = json_decode($values[$field]);
                 if (!is_array($values)) {
