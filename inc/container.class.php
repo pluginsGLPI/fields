934435b0ee756524deafbb5c927ffc96099429d6
Romain B. <8530352+Rom1-B@users.noreply.github.com>
2025-10-10 14:26:30 +0200

    Apply suggestion from @Rom1-B

--- inc/container.class.php
+++ inc/container.class.php
@@ -31,33 +31,6 @@
 use Glpi\Features\Clonable;
 use Glpi\DBAL\QueryExpression;
 
-/**
- * -------------------------------------------------------------------------
- * Fields plugin for GLPI
- * -------------------------------------------------------------------------
- *
- * LICENSE
- *
- * This file is part of Fields.
- *
- * Fields is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * Fields is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with Fields. If not, see <http://www.gnu.org/licenses/>.
- * -------------------------------------------------------------------------
- * @copyright Copyright (C) 2013-2023 by Fields plugin team.
- * @license   GPLv2 https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/pluginsGLPI/fields
- * -------------------------------------------------------------------------
- */
 class PluginFieldsContainer extends CommonDBTM
 {
     use Clonable;
