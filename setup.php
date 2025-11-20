d1cfed3d794b50790dd7d4ec4d50c88a20a4aa8b
Stanislas Kita <skita@teclib.com>
2025-11-20 11:14:43 +0100

    1.23.1

--- setup.php
+++ setup.php
@@ -31,7 +31,7 @@
 /** @var array $CFG_GLPI */
 global $CFG_GLPI;
 
-define('PLUGIN_FIELDS_VERSION', '1.23.0');
+define('PLUGIN_FIELDS_VERSION', '1.23.1');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_FIELDS_MIN_GLPI', '11.0.2');
