8efab96115297c873197422ab7d5aef0ac0d24ac
MyuTsu <gaetan.langlois31@gmail.com>
2026-02-05 14:16:35 +0100

    fix

--- inc/field.class.php
+++ inc/field.class.php
@@ -1278,7 +1278,6 @@ JAVASCRIPT,
                 // - either from a previous input (it will be an array).
                 //
                 // -> Decode it only if it is not already an array.
-                $value = json_decode((string) $value);
                 $decoded = json_decode((string) $value, true);
                 $value = is_array($decoded) ? $decoded : [];
             }
