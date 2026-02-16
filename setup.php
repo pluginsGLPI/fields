f805c2c0ee469f57a22c1e517f60bf1aa9cf2d0a
Rom1-B <rom1.biot@gmail.com>
2026-02-16 09:31:40 +0100

    1.21.28

--- setup.php
+++ setup.php
@@ -28,7 +28,7 @@
  * -------------------------------------------------------------------------
  */
 
-define('PLUGIN_FIELDS_VERSION', '1.21.27');
+define('PLUGIN_FIELDS_VERSION', '1.21.28');
 
 // Minimal GLPI version, inclusive
 define('PLUGIN_FIELDS_MIN_GLPI', '10.0.11');
