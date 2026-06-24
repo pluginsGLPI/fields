81e6e2c7707b4f86438d1675f5bb47f2077abf4b
Marie Bressy <75746914+Mary-Clb@users.noreply.github.com>
2026-06-24 10:10:43 +0200

    fix(js): prevent refreshContainer crash when fields dropdown values mix scalar and array inputs (#1206)
    * handle mixed field values
    
    * update CHANGELOG.md
    
    ---------
    
    Co-authored-by: Stanislas <skita@teclib.com>

--- inc/field.class.php
+++ inc/field.class.php
@@ -1013,6 +1013,8 @@ class PluginFieldsField extends CommonDBChild
                             var name = multiple_matches[1];
                             if (!(name in obj) || obj[name] == "") {
                                 obj[name] = [];
+                            } else if (!Array.isArray(obj[name])) {
+                                obj[name] = [obj[name]];
                             }
                             obj[name].push(item.value);
                         } else {
